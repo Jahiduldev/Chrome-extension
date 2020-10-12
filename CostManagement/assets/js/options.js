fdb437144709dbbd911d321a1d8f98b5b0c01d74
jahiduldev <jahiduldev@gmail.com>
2020-10-13 04:31:54 +0600

    cost managing software

--- CostManagement/assets/js/options.js
+++ CostManagement/assets/js/options.js
@@ -1,1 +1,26 @@
-options.js
+$(function(){
+
+	
+	$('#saveLimit').click(function(){ 
+
+		var limit = $('#limit').val();
+		if (limit) {
+
+			chrome.storage.sync.set({"limit": limit}, function(){
+
+				// console.log(limit);
+				close();
+			});
+		}
+
+		
+	});
+
+
+	$('#resetTotal').click(function(){
+
+		chrome.storage.sync.set({"total":0});
+
+	});
+
+});
--- CostManagement/assets/js/popup.js
+++ CostManagement/assets/js/popup.js
@@ -1,33 +1,40 @@
 $(function(){
 
-	chrome.storage.sync.get('total', function(budget){
-		$('#total').text(budget.total);
-	})
+    chrome.storage.sync.get(['total','limit'],function(budget){
+        $('#total').text(budget.total);
+        $('#limit').text(budget.limit);
+    });
 
+    $('#spendAmount').click(function(){
+        chrome.storage.sync.get(['total', 'limit'],function(budget){
+            var newTotal = 0;
+            if (budget.total){
+                newTotal += parseInt(budget.total);
+            }
 
-	$('#spendAmount').click(function(){ 
+            var amount = $('#amount').val();
+            if (amount){
+                newTotal += parseInt(amount);
+            }
 
-		chrome.storage.sync.get('total',function(budget){
+            chrome.storage.sync.set({'total': newTotal}, function(){  
+                         
+                if (amount && newTotal >= budget.limit){
+                    var notifOptions = {
+                        type: "basic",
+                        iconUrl: "../assets/images/sarahma.png",
+                        title: "Limit reached!",
+                        message: "Uh oh, look's like you've reached your alloted limit."
+                    };
+                    chrome.notifications.create('limitNotif', notifOptions);
 
-			var newTotal = 0;
-			if(budget.total){
-				newTotal += parseInt(budget.total);
+            }
+            });
+            $('#total').text(newTotal);
+            $('#amount').val('');
 
-			}
+           
 
-			var amount = $('#amount').val();
-			if (amount) {
-				newTotal += parseInt(amount);
-			} 
-
-			chrome.storage.sync.set({'total': newTotal});
-
-			$('#total').text(newTotal);
-			$('#amount').val('');
-
-
-		})
-
-
-	})
-})
+        });
+    });
+});
